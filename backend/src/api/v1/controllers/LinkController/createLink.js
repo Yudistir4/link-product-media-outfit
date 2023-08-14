a61cbf5782c33dcb3848764d24f65b2e2a165c45
Yudistira Maulana <yudistir4maulana@gmail.com>
2023-08-14 10:38:24 +0700

    disable convert

--- backend/src/api/v1/controllers/LinkController/createLink.js
+++ backend/src/api/v1/controllers/LinkController/createLink.js
@@ -4,18 +4,18 @@ const {
   customRespond,
   // errorRespond,
 } = require("../../utils/respondUtils");
-const { convert } = require("../../utils/shopeeUtils");
+// const { convert } = require("../../utils/shopeeUtils");
 const { tryCatch } = require("../../utils/tryCatch");
 
 exports.createLink = tryCatch(async (req, res) => {
   let data = new Link(req.body);
-  for (let i = 0; i < data.links.length; i++) {
-    data.links[i].link = await convert(
-      data.links[i].link,
-      i + 1,
-      req.body.tags
-    );
-  }
+  // for (let i = 0; i < data.links.length; i++) {
+  //   data.links[i].link = await convert(
+  //     data.links[i].link,
+  //     i + 1,
+  //     req.body.tags
+  //   );
+  // }
 
   data = await data.save();
   successRespond(res, customRespond.RecordCreated, data);
